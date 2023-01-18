cda6fd4363018e06d944affb039b3b27aac59743
Lutz Prechelt <prechelt@inf.fu-berlin.de>
2023-01-18 11:25:37 +0100

    ScihubMapper: extract base URL as constant (it changes all the time)

--- src/retrievelit/doi_pdf_mappers/scihub.py
+++ src/retrievelit/doi_pdf_mappers/scihub.py
@@ -4,9 +4,11 @@ from retrievelit.doi_pdf_mappers.abstract_doi_mapper import DoiMapper
 
 logger = logging.getLogger(__name__)
 
+SCIHUB_URL = 'https://sci.bban.top'  # changes frequently, one must correct it manually
+
 class ScihubMapper(DoiMapper):
     """Get the PDF download URL for DOIs using Scihub."""
     def get_pdf_url(self, doi: str) -> str:
-        url = f'https://sci.bban.top/pdf/{doi}.pdf#view=FitH'
+        url = f'{SCIHUB_URL}/pdf/{doi}.pdf#view=FitH'
         logger.debug(f'Built PDF URL {url}')
         return url
