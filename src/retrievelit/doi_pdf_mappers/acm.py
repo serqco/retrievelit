dbb820c87591bff57fc2a0257b184176d8a5c310
Lutz Prechelt <prechelt@inf.fu-berlin.de>
2023-01-19 18:09:20 +0100

    AcmMapper: get_pdfdescriptor

--- src/retrievelit/doi_pdf_mappers/acm.py
+++ src/retrievelit/doi_pdf_mappers/acm.py
@@ -1,12 +1,19 @@
 import logging
 
-from retrievelit.doi_pdf_mappers.base import DoiMapper
+from retrievelit.doi_pdf_mappers.base import DoiMapper, PDFDescriptor
 
 logger = logging.getLogger(__name__)
 
 class AcmMapper(DoiMapper):
     """Get the PDF download URL for DOIs resolving to ACM domains."""
+    DL_LINK_BASE = "https://dl.acm.org/doi/pdf"
+
     def get_pdf_url(self, doi: str) -> str:
-        url = f'https://dl.acm.org/doi/pdf/{doi}'
+        url = f'{self.DL_LINK_BASE}/{doi}'
         logger.debug(f'Built PDF URL {url}')
         return url
+
+    def get_pdfdescriptor(self, doi: str) -> PDFDescriptor:
+        dl_link = self.get_pdf_url(doi)
+        filename = "%s.pdf" % doi.split('/')[1]
+        return PDFDescriptor(dl_link, filename)
