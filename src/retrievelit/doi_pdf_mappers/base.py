53e6ac9888fa8bb77f0d73eb33e243cc0a495aa4
Lutz Prechelt <prechelt@inf.fu-berlin.de>
2023-01-19 17:23:28 +0100

    DoiMapper.get_pdfdescriptor, PDFDescriptor

--- src/retrievelit/doi_pdf_mappers/base.py
+++ src/retrievelit/doi_pdf_mappers/base.py
@@ -1,6 +1,13 @@
+from dataclasses import dataclass
 from abc import ABC, abstractmethod
 
 DOI_URL_PREFIX = 'https://doi.org/'
+
+
+@dataclass
+class PDFDescriptor:
+    download_url: str
+    filename: str
 
 
 def doi_url(doi: str) -> str:
@@ -12,4 +19,9 @@ class DoiMapper(ABC):
     @abstractmethod
     def get_pdf_url(self, doi: str) -> str:
         """Return the PDF download URL for the DOI."""
+        pass
+
+    @abstractmethod
+    def get_pdfdescriptor(self, doi: str) -> PDFDescriptor:
+        """Return PDF download URL and filename for the DOI."""
         pass
