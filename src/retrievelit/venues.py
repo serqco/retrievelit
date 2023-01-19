e1c000ebcebc29883569cf4f9720c560c0f32b8d
Lutz Prechelt <prechelt@inf.fu-berlin.de>
2023-01-19 12:00:30 +0100

    venues.py: add EnCyCris for testing

--- src/retrievelit/venues.py
+++ src/retrievelit/venues.py
@@ -10,6 +10,16 @@ VENUES = {
             },
         },
     },
+    'EnCyCris': {  # small venue, included for testing purposes
+        'name': 'Workshop on Engineering and Cybersecurity of Critical Systems (EnCyCriS)',
+        'type': 'conference',
+        'metadata_sources': {
+            'dblp': {
+                'type': 'conf',
+                'acronym': 'icse-encycris'
+            },
+        },
+    },
     'ICSE': {
         'name': 'International Conference on Software Engineering',
         'type': 'conference',
