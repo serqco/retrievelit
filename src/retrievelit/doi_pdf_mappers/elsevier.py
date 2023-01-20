44836bef2327ed2c7a07d7a30ce155664a93b4b8
LloydM <dev@lloydm.io>
2023-01-20 16:30:01 +0100

    Updated ElsevierMapping to download proper URL
    The Elsevier PDF download URL is constructed from the resolved DOI link. Once resolved, it is integrated into the download link.

--- src/retrievelit/doi_pdf_mappers/elsevier.py
+++ src/retrievelit/doi_pdf_mappers/elsevier.py
@@ -1,4 +1,5 @@
 import logging
+import pydoi
 
 from retrievelit.doi_pdf_mappers.base import DoiMapper, PDFDescriptor
 from retrievelit.exceptions import PdfUrlNotFoundError
@@ -11,15 +12,15 @@ class ElsevierMapper(DoiMapper):
 
     def get_pdf_url(self, doi: str) -> str:
         logger.debug(f'get_pdf_url({doi})')
-        ids = doi.split('/')[-1]
-        if not ids:
+        elsevier_id = pydoi.get_url(doi).split('/')[-1]
+        if not elsevier_id:
             raise PdfUrlNotFoundError("Url doesn't match expected pattern.")
-        url = f'{self.DL_LINK_BASE}/{ids}/pdfft?isDTMRedir=true&download=true'
+        url = f'{self.DL_LINK_BASE}/{elsevier_id}/pdfft?isDTMRedir=true&download=true'
         logger.debug(f'Built PDF URL {url}')
         return url
 
     def get_pdfdescriptor(self, doi: str) -> PDFDescriptor:
         pdf_dl_url = self.get_pdf_url(doi)
-        elsevier_id = doi.split('/')[1]  # part 2 of DOI is used in filename
+        elsevier_id = pydoi.get_url(doi).split('/')[-1]  # part 2 of DOI is used in filename
         download_filename = f'1-s2.0-{elsevier_id}-main.pdf'
         return PDFDescriptor(pdf_dl_url, download_filename)
